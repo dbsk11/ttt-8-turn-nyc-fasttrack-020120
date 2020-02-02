46e1d81ba51ea43bf019a958d2284d48e80eaeaf
balanced-widget-4002 <jessikim807@gmail.com>
2020-02-02 18:54:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,4 +17,6 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if !position_taken? && user_input.between?(1,9_)
+  if !position_taken? && user_input.between?(1,9)
+  end
+end
